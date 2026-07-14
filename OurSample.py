34d80b4218f9476ba4629ee83526e3baf51b5727
Adelanke <94877657+Ade-Lankie@users.noreply.github.com>
2026-07-14 08:24:43 +0100

    Update OurSample.py

--- OurSample.py
+++ OurSample.py
@@ -1,10 +1,10 @@
 echo "# QuizGame" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Ade-Lankie/QuizGame.git
-git push -u origin main
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Ade-Lankie/QuizGame.git
+# git push -u origin main
 
 import streamlit as st
 import random
