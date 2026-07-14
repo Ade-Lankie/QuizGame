57cf698a50a908cb39d7cf5d7b6547464aef2fd5
Adelanke <94877657+Ade-Lankie@users.noreply.github.com>
2026-07-14 14:30:20 +0100

    Update OurSample.py

--- OurSample.py
+++ OurSample.py
@@ -1,18 +1,92 @@
-# echo "# QuizGame" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/Ade-Lankie/QuizGame.git
-# git push -u origin main
-
 import streamlit as st
 import random
 import time
-#from streamlit_autorefresh import st_autorefresh
+# from streamlit_autorefresh import st_autorefresh
 
 # Refresh the page every second
-#st_autorefresh(interval=1000, key="refresh")
+# st_autorefresh(interval=1000, key="refresh")
+
+st.title("🎮 Python Quiz Challenge")
+
+
+
+
+
+if st.button("Start Game"):
+    st.session_state.started = True
+    st.rerun()
+
+if st.button("Restart"):
+
+    for key in list(st.session_state.keys()):
+        del st.session_state[key]
+
+    st.rerun()
+
+
+
+# -------------------------
+# Number Guessing Game
+# -------------------------
+if "game_stage" not in st.session_state:
+    st.session_state.game_stage = "guess"
+
+if "secret_number" not in st.session_state:
+    st.session_state.secret_number = random.randint(1, 10)
+
+if "attempts" not in st.session_state:
+    st.session_state.attempts = 3
+
+
+# -------------------------
+# Stage 1 - Number Guessing Game
+# -------------------------
+if st.session_state.game_stage == "guess":
+
+    st.header(" Number Guessing Challenge")
+    st.write("Guess a number between **1 and 10**.")
+    st.write(f"You have **{st.session_state.attempts}** attempts remaining.")
+
+    guess = st.number_input(
+        "Enter your guess",
+        min_value=1,
+        max_value=100,
+        step=1,
+        key="guess"
+    )
+
+    if st.button("Guess Number"):
+
+        if guess == st.session_state.secret_number:
+            st.success("🎉 Congratulations! You guessed correctly.")
+            st.success("You have unlocked the Quiz Game!")
+
+            st.session_state.game_stage = "quiz"
+            st.rerun()
+
+        else:
+            st.session_state.attempts -= 1
+
+            if st.session_state.attempts == 0:
+                st.error("❌ Game Over!")
+                st.write(f"The correct number was **{st.session_state.secret_number}**.")
+                st.stop()
+
+            elif guess < st.session_state.secret_number:
+                st.warning("⬆️ Too low! Try a higher number.")
+
+            else:
+                st.warning("⬇️ Too high! Try a lower number.")
+
+    st.stop()
+
+
+
+
+
+
+
+
 
 # Quiz questions
 quiz = (
@@ -23,7 +97,7 @@ quiz = (
     ("How many days are there in a leap year?", "366")
 )
 
-st.title(" Python Quiz Game")
+st.title("Python Quiz Game")
 
 # -------------------------
 # Initialize session state
